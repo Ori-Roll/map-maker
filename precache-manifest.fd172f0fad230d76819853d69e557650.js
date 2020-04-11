ce7632d2267b49961897352216657a2129c52ceb
Ori Roll <oriroll@gmail.com>
2020-04-11 10:35:19 +0300

    Updates

---
+++ precache-manifest.fd172f0fad230d76819853d69e557650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5717fc8c355af05839460d4851dd270a",
+    "url": "/map-maker/index.html"
+  },
+  {
+    "revision": "989500dd587e58eab193",
+    "url": "/map-maker/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1f31191631231ac71ec8",
+    "url": "/map-maker/static/js/2.4dc4ade8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/map-maker/static/js/2.4dc4ade8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "989500dd587e58eab193",
+    "url": "/map-maker/static/js/main.9b3e6a0a.chunk.js"
+  },
+  {
+    "revision": "d93ad71de357348f90fe",
+    "url": "/map-maker/static/js/runtime-main.74247e05.js"
+  }
+]);
